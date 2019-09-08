542e6d6da93f723c74af92e5a09ba527c4f593c6
Sergio Contreras <checo1085@hotmail.com>
2019-09-08 16:50:12 -0700

    Implemented generatewhere in gendry builder

--- models/gendry/filters.go
+++ models/gendry/filters.go
@@ -1,10 +1,61 @@
 package gendry
 
 import (
+	"fmt"
+
 	"github.com/Demitroi/query-builders/models"
+	"github.com/pkg/errors"
 )
 
-func (qb *queryBuilder) GenerateFilter(filter models.Filter) (m map[string]interface{}, err error) {
-
-	return m, nil
+func (qb *queryBuilder) GenerateWhere(filter models.Filter) (where map[string]interface{}, err error) {
+	err = filter.ForEach(func(field string, operator string, value interface{}) error {
+		switch operator {
+		case "=":
+			k := fmt.Sprintf("%s =", field)
+			where[k] = value
+		case ">":
+			k := fmt.Sprintf("%s >", field)
+			where[k] = value
+		case "<":
+			k := fmt.Sprintf("%s <", field)
+			where[k] = value
+		case "<=":
+			k := fmt.Sprintf("%s <=", field)
+			where[k] = value
+		case ">=":
+			k := fmt.Sprintf("%s >=", field)
+			where[k] = value
+		case "!=":
+			k := fmt.Sprintf("%s !=", field)
+			where[k] = value
+		case "<>":
+			k := fmt.Sprintf("%s <>", field)
+			where[k] = value
+		case "in":
+			k := fmt.Sprintf("%s in", field)
+			where[k] = value
+		case "not in":
+			k := fmt.Sprintf("%s not in", field)
+			where[k] = value
+		case "like":
+			k := fmt.Sprintf("%s like", field)
+			where[k] = value
+		case "not like":
+			k := fmt.Sprintf("%s not like", field)
+			where[k] = value
+		case "between":
+			k := fmt.Sprintf("%s between", field)
+			where[k] = value
+		case "not between":
+			k := fmt.Sprintf("%s not between", field)
+			where[k] = value
+		default:
+			return errors.Errorf("operator %s not supported", operator)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return where, nil
 }
