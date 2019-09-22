65ad79416699ba17fccbe6194abca00dfd3651a3
Sergio Contreras <checo1085@hotmail.com>
2019-09-22 16:56:00 -0700

    Added method and test to addperson in dbx

--- models/ozzo-dbx/persons.go
+++ models/ozzo-dbx/persons.go
@@ -2,10 +2,21 @@ package dbx
 
 import (
 	"github.com/Demitroi/query-builders/models"
+	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
-func (*queryBuilder) AddPerson(person models.Person) (lastID string, err error) {
-	return
+func (qb *queryBuilder) AddPerson(person models.Person) (lastID string, err error) {
+	m := person.ToMap()
+	res, err := qb.database.Insert("persons", m).Execute()
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to exec query")
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to get lastid")
+	}
+	return cast.ToString(id), nil
 }
 
 func (*queryBuilder) GetPerson(id string) (person models.Person, err error) {
