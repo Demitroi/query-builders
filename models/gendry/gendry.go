415c62da64754526aae1db14f982b6d5dcb5a360
Sergio Contreras <checo1085@hotmail.com>
2020-01-24 20:29:51 -0700

    Se gendry tag name

--- models/gendry/gendry.go
+++ models/gendry/gendry.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/Demitroi/query-builders/models"
+	"github.com/didi/gendry/scanner"
 )
 
 type queryBuilder struct {
@@ -13,5 +14,6 @@ type queryBuilder struct {
 
 // New creates query builder
 func New(db *sql.DB) models.QueryBuilder {
+	scanner.SetTagName("db")
 	return &queryBuilder{db}
 }
