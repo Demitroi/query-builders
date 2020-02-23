3832496ad19cba6498705acad3d48aab8adfebb4
Sergio Contreras <checo1085@hotmail.com>
2020-02-23 14:48:43 -0700

    test put persons

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -407,3 +407,154 @@ func TestAddPersonInternalServerError(t *testing.T) {
 	handlers.QueryBuilder = dbx.New(db)
 	fn()
 }
+
+func TestUpdatePerson(t *testing.T) {
+	// Create database mock
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	// Create iris app
+	app := iris.New()
+	// Create party an register routes in root path
+	party := app.Party("/")
+	handlers.RegisterPersons(party)
+	// build the iris app
+	err = app.Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn := func() {
+		// Request PUT persons must exec an insert
+		result := sqlmock.NewResult(0, 1)
+		mock.ExpectExec("UPDATE").
+			WillReturnResult(result)
+		// Create recorder and serve
+		w := httptest.NewRecorder()
+		// Create body reader
+		body := strings.NewReader(`{
+			"name": "Ash Ketchum",
+			"city": "Pallet Town",
+			"birth_date": "2006-01-02T15:04:05Z",
+			"weight": 91,
+			"height": 81
+		}`)
+		// Create a request
+		req, err := http.NewRequest(http.MethodPut, "/persons/1", body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Do the request and catch the response code and body
+		app.ServeHTTP(w, req)
+		if w.Code != http.StatusOK {
+			t.Errorf("%T %v %s", handlers.QueryBuilder, w.Code, w.Body.String())
+			return
+		}
+	}
+	// Create and assign the query builders
+	handlers.QueryBuilder = goqu.New(db)
+	fn()
+	handlers.QueryBuilder = gendry.New(db)
+	fn()
+	handlers.QueryBuilder = dbx.New(db)
+	fn()
+}
+
+func TestUpdatePersonBadRequest(t *testing.T) {
+	// Create database mock
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	// Create iris app
+	app := iris.New()
+	// Create party an register routes in root path
+	party := app.Party("/")
+	handlers.RegisterPersons(party)
+	// build the iris app
+	err = app.Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn := func() {
+		// Request PUT persons must exec an insert
+		result := sqlmock.NewResult(0, 1)
+		mock.ExpectExec("UPDATE").
+			WillReturnResult(result)
+		// Create recorder and serve
+		w := httptest.NewRecorder()
+		// Create a request
+		req, err := http.NewRequest(http.MethodPut, "/persons/1", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Do the request and catch the response code and body
+		app.ServeHTTP(w, req)
+		if w.Code != http.StatusBadRequest {
+			t.Errorf("%T %v %s", handlers.QueryBuilder, w.Code, w.Body.String())
+			return
+		}
+	}
+	// Create and assign the query builders
+	handlers.QueryBuilder = goqu.New(db)
+	fn()
+	handlers.QueryBuilder = gendry.New(db)
+	fn()
+	handlers.QueryBuilder = dbx.New(db)
+	fn()
+}
+
+func TestUpdatePersonInternalServerError(t *testing.T) {
+	// Create database mock
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	// Create iris app
+	app := iris.New()
+	// Create party an register routes in root path
+	party := app.Party("/")
+	handlers.RegisterPersons(party)
+	// build the iris app
+	err = app.Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fn := func() {
+		err = errors.New("This is an error")
+		// Request PUT persons must exec an insert
+		mock.ExpectExec("UPDATE").
+			WillReturnError(err)
+		// Create recorder and serve
+		w := httptest.NewRecorder()
+		// Create body reader
+		body := strings.NewReader(`{
+			"name": "Ash Ketchum",
+			"city": "Pallet Town",
+			"birth_date": "2006-01-02T15:04:05Z",
+			"weight": 91,
+			"height": 81
+		}`)
+		// Create a request
+		req, err := http.NewRequest(http.MethodPut, "/persons/1", body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Do the request and catch the response code and body
+		app.ServeHTTP(w, req)
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("%T %v %s", handlers.QueryBuilder, w.Code, w.Body.String())
+			return
+		}
+	}
+	// Create and assign the query builders
+	handlers.QueryBuilder = goqu.New(db)
+	fn()
+	handlers.QueryBuilder = gendry.New(db)
+	fn()
+	handlers.QueryBuilder = dbx.New(db)
+	fn()
+}
