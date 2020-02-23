2897486b1f40e60c000ecb7f510cf27902f36657
Sergio Contreras <checo1085@hotmail.com>
2020-02-23 13:31:56 -0700

    Test get persons with all query builders

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -8,7 +8,9 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/Demitroi/query-builders/handlers"
+	"github.com/Demitroi/query-builders/models/gendry"
 	"github.com/Demitroi/query-builders/models/goqu"
+	dbx "github.com/Demitroi/query-builders/models/ozzo-dbx"
 	"github.com/kataras/iris/v12"
 )
 
@@ -19,19 +21,12 @@ func TestGetPersons(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
-	// Create and assign the query builder
-	handlers.QueryBuilder = goqu.New(db)
 	// Create iris app
 	app := iris.New()
 	// Create party an register routes in root path
 	party := app.Party("/")
 	handlers.RegisterPersons(party)
-	// Create a request
-	req, err := http.NewRequest(http.MethodGet, "/persons", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// important
+	// build the iris app
 	err = app.Build()
 	if err != nil {
 		t.Fatal(err)
@@ -40,12 +35,30 @@ func TestGetPersons(t *testing.T) {
 	columns := []string{"id", "name", "city", "birth_date", "weight", "height"}
 	// Parse birthdate
 	birthDate, err := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
-	// Must query
-	mock.ExpectQuery("SELECT `id`, `name`, `city`, `birth_date`, `weight`, `height`").
-		WithArgs("1").
-		WillReturnRows(sqlmock.NewRows(columns).
-			AddRow("1", "Ash Ketchum", "Pallet Town", birthDate, float32(91), float32(1.81)))
-	// Create recorder and serve
-	w := httptest.NewRecorder()
-	app.ServeHTTP(w, req)
+	fn := func() {
+		// Request GET persons must query a select
+		mock.ExpectQuery("SELECT").
+			WillReturnRows(sqlmock.NewRows(columns).
+				AddRow("1", "Ash Ketchum", "Pallet Town", birthDate, float32(91), float32(1.81)))
+		// Create recorder and serve
+		w := httptest.NewRecorder()
+		// Create a request
+		req, err := http.NewRequest(http.MethodGet, "/persons", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Do the request and catch the response code and body
+		app.ServeHTTP(w, req)
+		if w.Code != http.StatusOK {
+			t.Errorf("%T %v %s", handlers.QueryBuilder, w.Code, w.Body.String())
+			return
+		}
+	}
+	// Create and assign the query builders
+	handlers.QueryBuilder = goqu.New(db)
+	fn()
+	handlers.QueryBuilder = gendry.New(db)
+	fn()
+	handlers.QueryBuilder = dbx.New(db)
+	fn()
 }
